4cafb412e7c7e66029dee2f81bc6ae2ab8b0cd32
Gaurav Tredia <tgaurav@cambium.co.il>
2022-01-22 16:31:50 +0200

    change in route

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
             </label>  
         </div>
       <Routes>
-        <Route path="https://trediagaurav.github.io/weatherForecast/" exactly element={<Home />} />
-        <Route path="https://trediagaurav.github.io/weatherForecast/favorite" element={<Fav />} />
-        <Route path="https://trediagaurav.github.io/weatherForecast/*" element={<Home />} />
+        <Route path="/weatherForecast" exactly element={<Home />} />
+        <Route path="/weatherForecast/favorite" element={<Fav />} />
+        <Route path="/weatherForecast/*" element={<Home />} />
       </Routes>
     </div>
   );
